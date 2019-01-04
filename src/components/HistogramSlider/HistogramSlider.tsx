4888eecf5e7d8b08f0904a91b147df87e4d03911
zlatan <zlatan.pham@gmail.com>
2019-01-04 10:55:57 +0700

    scaffolding

---
+++ src/components/HistogramSlider/HistogramSlider.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Histogram } from './Histogram';
+import { Slider } from './Slider';
+
+export const HistogramSlider = () => (
+  <div>
+    <Histogram />
+    <Slider />
+  </div>
+);
